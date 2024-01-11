780ab3843ada2efcdafdd61ab683131a2b291780
Avilash M <hi.avilashm@gmail.com>
2024-01-12 00:11:46 +0530

    React Router

---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+function User() {
+    const {userId} = useParams();
+
+  return (
+    <div className='bg-gray-600 text-white text-3xl p-4 text-center'>User : {userId} </div>
+  )
+}
+
+export default User
